36ea1045ce09eb5c4c1f7cf1a9e8ee55b35ceb44
Marcelo Urbano <72034608+Marcelourb@users.noreply.github.com>
2021-03-19 21:26:19 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*alert box example*/
+
+function windowalert() {
+    alert("Yes I'm an alert box and I can do more than say that 'I'm an alert box'. Instead of it, I'm here to remind you to drink water. Go on and be hydrated!");   
+}
+
+/*light bulb example*/ 
+
+function lightOff() {
+
+    document.getElementById("light-bulb").src='img/lightbulb-off.jpg';
+    
+    }
+    
+    function lightOn() {
+    
+    document.getElementById("light-bulb").src='img/lightbulb-on.jpg';
+    
+    }
+
